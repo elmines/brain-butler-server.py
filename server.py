3baf10beba056e898627fbca355c40950ccfdda7
Ethan Mines <elmines325@gmail.com>
2019-01-11 15:37:05 -0600

    Open and close JSON file in sync with data stream

--- server.py
+++ server.py
@@ -8,61 +8,74 @@ lan_ip = netifaces.ifaddresses("wlo1")[netifaces.AF_INET][0]["addr"]
 port = 8080
 print("Serving from ws://{}:{}".format(lan_ip, port))
 
-def new_outfile():
-	filename = datetime.datetime.now().strftime("%Y_%m_%d_%I:%M:%S") + ".json"
-	print(f"Opening outfile {filename}")
-	return open(filename, "w")
 
-curr_file = None
-header_provided = None
-data_records = 0
-def reset_globals():
-	global curr_file
-	global header_provided
-	global data_records
+class Server(object):
+	"""
+	A single-threaded server for receiving data packets
+	"""
 
-	if curr_file: end_file(curr_file)
-	curr_file = None
-	header_provided = False
-	data_records = 0
-reset_globals()
+	def __init__(self):
+		self.curr_file = None
+		self.header_provided = None
+		self.data_records = 0
 
-def begin_file():
-	global curr_file
-	curr_file = new_outfile()
-	curr_file.write("{")
-	return curr_file
-def end_file(data_file):
-	data_file.write("]") #End data
-	data_file.write("}") #End root object
-	data_file.close()
-	print(f"Closed outfile {data_file.name}")
+	def receive_packet(self, packet):
+		"""
+		Description
 
+		:param dict(str, obj) packet: The header, data, etc., packet
+		"""
+		if packet["type"] == "header":
+			self._reset_globals()
+			self.header_provided = True
+
+			self.curr_file = Server._begin_file()
+			self.curr_file.write('\"header\":{}'.format( json.dumps(packet["body"]) ))
+			self.curr_file.write(', \"data\":[')
+			
+		elif packet["type"] == "eof":
+			self._reset_globals()
+		else:
+			if not self.header_provided:
+				print("Header has not been provided--rejecting data record")
+				return
+
+			if self.data_records > 0: self.curr_file.write(",")
+			self.curr_file.write( json.dumps(packet["body"])[1:-1] )	
+			self.data_records += 1
+
+
+	#PRIVATE
+	def _reset_globals(self):
+		if self.curr_file: Server._end_file(self.curr_file)
+		self.curr_file = None
+		self.header_provided = False
+		self.data_records = 0
+	@staticmethod
+	def _begin_file():
+		curr_file = Server._new_outfile()
+		curr_file.write("{") #Begin root object
+		return curr_file
+	@staticmethod
+	def _new_outfile():
+		filename = datetime.datetime.now().strftime("%Y_%m_%d_%I:%M:%S") + ".json"
+		print(f"Opening outfile {filename}")
+		return open(filename, "w")
+	@staticmethod
+	def _end_file(data_file):
+		data_file.write("]") #End data
+		data_file.write("}") #End root object
+		data_file.close()
+		print(f"Closed outfile {data_file.name}")
+	
+		
+		
+bb_server = Server()
 async def print_message(websocket, path):
-	global curr_file
-	global header_provided
-	global data_records
-
+	global bb_server
 	async for message in websocket:
 		json_obj = json.loads(message)
-		if json_obj["type"] == "header":
-			header_provided = True
-			reset_globals()
-
-			curr_file = begin_file()
-			curr_file.write('\"header\":{}'.format( json.dumps(json_obj["body"]) ))
-			curr_file.write(', \"data\":[')
-			
-		elif json_obj["type"] == "eof":
-			reset_globals()
-		else:
-			if not header_provided:
-				print("Header has not been provided--rejecting data record")
-				continue
-
-			if data_records > 0: curr_file.write(",")
-			curr_file.write( json.dumps(json_obj["body"])[1:-1] )	
-			data_records += 1
+		bb_server.receive_packet(json_obj)
 			
 
 asyncio.get_event_loop().run_until_complete( websockets.serve(print_message, lan_ip, port) )
