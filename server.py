ffafd3cdd1f99adff81b12a80e5fbec8dd712df8
Ethan Mines <elmines325@gmail.com>
2019-02-07 17:53:29 -0600

    Train classifier online

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import json
 import datetime
+#External libraries
+import tensorflow as tf
+import numpy as np
 #Local libraries
 import eeg
 import rl
+import erp
 
 class Server(object):
 	"""
@@ -24,8 +28,9 @@ class Server(object):
 		if packet["type"] == "header":
 			self._reset_members()
 			self.header_provided = True
-
-			self.agent = rl.Bandit()
+			self.sess = tf.Session()
+			self.model = erp.ml.convolution( [256, 4], 2)
+			self.sess.run(tf.global_variables_initializer())
 
 			self.curr_file = Server._begin_file()
 			self.curr_file.write('\"header\":{}'.format( json.dumps(packet["body"]) ))
@@ -42,12 +47,6 @@ class Server(object):
 			return self._processData(packet)
 		if packet["type"] == "event":
 			return self._processEvent(packet)
-		if packet["type"] == "reward":
-			id = packet["body"]["id"]
-			r = packet["body"]["r"]
-			print(f"Reward for action {id}: {r}")
-			self.agent.reward(id, r)
-			return None
 
 		print(f'Received packet of unknown type {packet["type"]}--rejecting')
 		return None
@@ -56,14 +55,36 @@ class Server(object):
 		if self.data_records > 0: self.curr_file.write(",")
 		self.curr_file.write( json.dumps(packet["body"])[1:-1] )
 		self.data_records += 1
-		if eeg.errp(packet["body"]):
-			if len(self.state) < 2:
-				print("State not yet fully initialized")
-			(id, action) = self.agent.act(self.state)
-			print(f"Taking action {id}: {action}")
-			return (id, action)
+
+		timestamp = packet["body"][0]["timestamp"]
+
+		print("Got a data record with timestamp", timestamp)
+
+		if "rotation" in self.last:
+			rotation = int( self.last["rotation"] >= timestamp )
+		else: rotation = 0
+		if "darkening" in self.last:
+			darkening = int( self.last["darkening"] >= timestamp )
+		else: darkening = 0
+
+		eegOnly    = np.array([ reading["data"] for reading in packet["body"]] )
+
+		act_inputs = np.expand_dims( erp.tools.cleanSample(eegOnly, 80, 256) , 0)
+		act_labels = np.expand_dims( np.array([rotation, darkening]), 0 )
+		print("act_labels =",act_labels)
+
+		sess = self.sess
+		model = self.model
+
+		(_, loss, predictions) = sess.run([model.train_op, model.loss, model.predictions],
+			{model.inputs: act_inputs, model.labels: act_labels}
+		)
+		print("loss =", loss)
+		print("predictions =", predictions)
+
 
 		return None
+
 
 	def _processEvent(self, packet):
 		if self.data_records > 0: self.curr_file.write(",")
@@ -71,18 +92,33 @@ class Server(object):
 		self.data_records += 1
 
 		eventName = packet["body"]["eventName"]
-		eventVal = packet["body"][eventName]
-		self.state[eventName] = eventVal
+		self.state[eventName] = packet["body"][eventName]
+		self.last[eventName] = packet["body"]["timestamp"]
+
+		print("Got an event record",packet["body"])
+
+		return None
 
 	#PRIVATE
 	def _reset_members(self):
 		if hasattr(self, "curr_file") and self.curr_file:
 			Server._end_file(self.curr_file)
 		self.curr_file = None
+
+		if hasattr(self, "sess") and self.sess:
+			self.sess.close()
+			tf.reset_default_graph()
+		self.sess = None
+		self.model = None
+
 		self.header_provided = False
 		self.data_records = 0
-		self.agent = None
 		self.state = {}
+		self.last = {}
+
+
+
+
 	@staticmethod
 	def _begin_file():
 		curr_file = Server._new_outfile()
